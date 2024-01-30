6d9fb32505a8f9fd802ea73b445621fda97f6f96
segun <segun408@gmail.com>
2024-01-30 10:59:32 +0900

    마이너스 콤마 추가

--- src/mixins.js
+++ src/mixins.js
@@ -22,7 +22,8 @@ export default {
       if (value !== undefined) {
         // 만약 100원 이하면 소수점 이하 표시
         if (value < 100) {
-          return value + " 원";
+          const formattedPrice = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          return formattedPrice + " 원";
         }
         // 소수점 제거
         value = Math.floor(value);
